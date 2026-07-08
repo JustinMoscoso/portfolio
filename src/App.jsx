b857f679231dbd8c5a8da329e26acbab7b91b0f0
Justin Moscoso <moscosojustin384@gmail.com>
2026-07-08 16:27:22 +0800

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import TypingTest from "./components/TypingTest";
 import Experience from "./pages/Experience";
+
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/typing-test" element={<TypingTest />} />
       <Route path="/experience" element={<Experience />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
